3f4b87b35ddd9c2c29c858f3041646b853afb81c
Jacek Kuzemczak <jacek@dataplan.co.uk>
2025-10-17 16:46:04 +0100

    Make ALE work like it used to

--- neovim/config/spec.lua
+++ neovim/config/spec.lua
@@ -1,25 +1,33 @@
 return {
-  { "mhinz/vim-signify" },
-  { "dense-analysis/ale" },
-  { "morhetz/gruvbox" },
-  { "kazhala/close-buffers.nvim" },
-  { "junegunn/fzf" },
-  { "junegunn/fzf.vim" },
-  { "SirVer/ultisnips" },
-  { "vim-vdebug/vdebug" },
-  { "editorconfig/editorconfig-vim" },
-  { "jeffkreeftmeijer/vim-numbertoggle" },
-  { "tpope/vim-obsession" },
-  { "sheerun/vim-polyglot" },
-  { "wesQ3/vim-windowswap" },
-  {
-    "windwp/windline.nvim",
-    config = function()
-      require("wlsample.airline")
-    end,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-  },
+    { "mhinz/vim-signify" },
+    {
+        "dense-analysis/ale",
+        config = function()
+            local g = vim.g
+
+            g.ale_php_phpcs_standard = "./ruleset.xml"
+            g.ale_use_neovim_diagnostics_api = 1
+        end,
+    },
+    { "morhetz/gruvbox" },
+    { "kazhala/close-buffers.nvim" },
+    { "junegunn/fzf" },
+    { "junegunn/fzf.vim" },
+    { "SirVer/ultisnips" },
+    { "vim-vdebug/vdebug" },
+    { "editorconfig/editorconfig-vim" },
+    { "jeffkreeftmeijer/vim-numbertoggle" },
+    { "tpope/vim-obsession" },
+    { "sheerun/vim-polyglot" },
+    { "wesQ3/vim-windowswap" },
+    {
+        "windwp/windline.nvim",
+        config = function()
+            require("wlsample.airline")
+        end,
+    },
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
 }
