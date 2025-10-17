4f9398b1175b6ea309a2e3ac7d59d1b559906ae4
Jacek Kuzemczak <jacek@dataplan.co.uk>
2025-10-17 12:37:50 +0100

    Migrate vim config to neovim

--- neovim/config/spec.lua
+++ neovim/config/spec.lua
@@ -1,3 +1,25 @@
 return {
-  { "mhinz/vim-signify" }
+  { "mhinz/vim-signify" },
+  { "dense-analysis/ale" },
+  { "morhetz/gruvbox" },
+  { "kazhala/close-buffers.nvim" },
+  { "junegunn/fzf" },
+  { "junegunn/fzf.vim" },
+  { "SirVer/ultisnips" },
+  { "vim-vdebug/vdebug" },
+  { "editorconfig/editorconfig-vim" },
+  { "jeffkreeftmeijer/vim-numbertoggle" },
+  { "tpope/vim-obsession" },
+  { "sheerun/vim-polyglot" },
+  { "wesQ3/vim-windowswap" },
+  {
+    "windwp/windline.nvim",
+    config = function()
+      require("wlsample.airline")
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
 }
